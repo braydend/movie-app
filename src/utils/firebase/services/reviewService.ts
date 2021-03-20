19a166897c2915806d7b19ae215945c1c6347636
Brayden <braydendiluca@gmail.com>
2021-03-20 17:26:15 +1100

    Simplify fetch reviews by id function

--- src/utils/firebase/services/reviewService.ts
+++ src/utils/firebase/services/reviewService.ts
@@ -1,60 +1,65 @@
-import firebase from '../../firebase';
+import firebase from "../../firebase";
 
 type Movie = {
-    IMDBId: string;
-    title: string;
+  IMDBId: string;
+  title: string;
 };
 
 type User = {
-    id: string;
-    name: string;
+  id: string;
+  name: string;
 };
 
 export type Review = {
-    score: number;
-    created: number;
-    message: string;
-    movie: Movie;
-    postedBy: User;
+  score: number;
+  created: number;
+  message: string;
+  movie: Movie;
+  postedBy: User;
 };
 
-export const createReview = async (score: number, message: string, title: string, IMDBId: string, user: firebase.User) => {
-    const id = firebase.db.collection('reviews').doc().id;
+export const createReview = async (
+  score: number,
+  message: string,
+  title: string,
+  IMDBId: string,
+  user: firebase.User
+) => {
+  const id = firebase.db.collection("reviews").doc().id;
 
-    if (!user.displayName) throw Error('User has no displayName');
+  if (!user.displayName) throw Error("User has no displayName");
 
-    const newReview: Review = {
-        score,
-        message,
-        movie: {
-            title,
-            IMDBId,
-        },
-        postedBy: {
-            id: user.uid,
-            name: user.displayName,
-        },
-        created: Date.now(),
-    };
-    await firebase.db.collection('reviews').doc(id).set(newReview);
+  const newReview: Review = {
+    score,
+    message,
+    movie: {
+      title,
+      IMDBId,
+    },
+    postedBy: {
+      id: user.uid,
+      name: user.displayName,
+    },
+    created: Date.now(),
+  };
+  await firebase.db.collection("reviews").doc(id).set(newReview);
 };
 
 export const getReviewsForMovie = async (id: string): Promise<Review[]> => {
-    const reviews = await firebase.db.collectionGroup('reviews').get();
+  const reviews = await firebase.db
+    .collectionGroup("reviews")
+    .where("movie.IMDBId", "==", id)
+    .get();
 
-    return reviews.docs.filter(doc => {
-        const data = doc.data();
+  return reviews.docs.map((_) => {
+    const data = _.data();
 
-        return data.movie.IMDBId === id;
-    }).map(_ => {
-        const data = _.data();
-        
-        return {
-            score: data.score,
-            message: data.message,
-            created: data.created,
-            movie: data.movie,
-            postedBy: data.postedBy,
-        };
-    });
+    return {
+      score: data.score,
+      message: data.message,
+      created: data.created,
+      movie: data.movie,
+      postedBy: data.postedBy,
+    };
+  });
 };
